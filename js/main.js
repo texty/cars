022b59970887fc504754b707fcf2253172db4da3
Vladislav <drimacus182@gmail.com>
2018-06-20 14:42:23 +0300

    невпевнена спроба зробити small multiples мультичарт

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-data_provider.getDailyData(function(daily_data) {
+data_provider.getDailyData(function(err, daily_data) {
+    if (err) throw err;
+
     console.log(daily_data);
     var total_chart = smallchart()
         .data(daily_data)
@@ -10,7 +12,9 @@ data_provider.getDailyData(function(daily_data) {
 
 });
 
-data_provider.getRegionsData(function(regions) {
+data_provider.getRegionsData(function(err, regions) {
+    if (err) throw err;
+
     var items = regions.map(function(d){
         return {label: d.name.replace(" область", ""), badge: d.n}
     });
@@ -24,7 +28,9 @@ data_provider.getRegionsData(function(regions) {
 });
 
 
-data_provider.getProducersData(function(producers) {
+data_provider.getProducersData(function(err, producers) {
+    if (err) throw err;
+
     var items = producers.map(function(d){
         return {label: d.producer, badge: d.n}
     });
@@ -35,4 +41,40 @@ data_provider.getProducersData(function(producers) {
         .items(items);
 
     d3.select('#producers_control').call(producers_control);
+});
+
+d3.queue()
+    .defer(data_provider.getRegionsData)
+    .defer(data_provider.getDailyData)
+    .await(function(err, regions, daily_data){
+        if (err) throw err;
+
+        var items = regions.map(function(d){
+            return {label: d.name.replace(" область", ""), badge: d.n}
+        });
+        
+        
+    
+        var charts_data = items;
+        
+        var small_multiples_chart = small_multiples()
+            .create_item(function(selection) {
+                var chart = smallchart()
+                    .data(daily_data)
+                    .varName("n");
+
+                selection.append("h3").text("область шмобласть")
+
+                selection.append("svg")
+                    .attr("class", "smallchart")
+                    .attr("width", "100%")
+                    .attr("data-aspect-ratio", "0.05")
+                    .attr("data-min-height", "72")
+                    .call(chart);
+            })
+            .update_item(function() {console.log("update item")})
+            .data(charts_data)
+        
+        d3.select('#small_multiples').call(small_multiples_chart);
+        small_multiples_chart.update();
 });
---
+++ js/small_multiples.js
@@ -0,0 +1,60 @@
+function small_multiples() {
+
+    var data
+        , create_item = noop
+        , update_item = noop
+        ;
+
+    function my(selection) {
+        selection.each(function(d) {
+
+            var container = d3.select(this);
+            
+            var items = container
+                .selectAll("div.small_multiples_item")
+                .data(data)
+                .enter()
+                .append("div")
+                .attr("class", "small_multiples_item")
+                .each(function(d){
+                    console.log("draw single item chart");
+                    d3.select(this).call(create_item);
+                });
+
+            function update() {
+                items
+                    .each(function(d){
+                        console.log("redraw single item chart");
+                        d3.select(this).call(update_item);
+                    });
+
+                return my;
+            }
+
+            my.update = update;
+        });
+    }
+
+    my.data = function(value) {
+        if (!arguments.length) return data;
+        data = value;
+        return my;
+    };
+
+    my.create_item = function(value) {
+        if (!arguments.length) return create_item;
+        create_item = value;
+        return my;
+    };
+
+
+    my.update_item = function(value) {
+        if (!arguments.length) return update_item;
+        update_item = value;
+        return my;
+    };
+    
+    function noop() {}
+
+    return my;
+}
